251c86d41b7cb648cf14bfef548e3c03cda03b49
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-11-25 19:14:24 +0530

    update: a simple minimal homepage

--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -1,17 +1,24 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Home = () => {
   return (
-    <div className='flex flex-col items-center justify-center container mx-auto h-screen'>
-      <div className='mb-5'>
-      a gorgeous landing page, lol
+    <div className="flex flex-col items-center justify-center container mx-auto h-screen">
+      <div className="mb-5">
+        <h1 className="text-4xl font-bold text-center text-gray-800 mb-4">
+          Welcome to eValauate (mvp)!
+        </h1>
+        <p className="text-lg text-center text-gray-600">
+          this is gorgeous landing page, lol
+        </p>
       </div>
-      <Link href={'/dashboard'}>
-        <span className='text-blue-500 underline underline-offset-4 font-light'>click here to goto dashboard</span>
+      <Link href={"/dashboard"}>
+        <span className="text-blue-500 underline underline-offset-4 font-light">
+          click here to goto dashboard
+        </span>
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
